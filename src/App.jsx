c9fa87aabdf8b491267a36fcc71d48ea527ebde7
Yildiz Yunus <yunusyildiz44800@gmail.com>
2023-12-25 19:33:37 +0100

    FEATURES : Ajout du bouton supprimé

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+//J'utilise la méthode .map dans une li pour afficher chaque nom sous forme de li.
+
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -6,27 +8,60 @@ import './App.css'
 function App() {
   //State
   const [titre, setTitre] = useState("Titre");
-  const [taches, setListes] = useState([
-    {id: 1, nom: 'tâche'},
-    {id: 2, nom: 'tâche'},
-    {id: 3, nom: 'tâche'}
+  const [taches, setTaches] = useState([
+    { id: 1, nom: 'tâche' },
+    { id: 2, nom: 'tâche' },
+    { id: 3, nom: 'tâche' }
   ]);
 
   //Comportements
+  const handleWrite = (event) => {
 
+    //1. On copie le tableau 
+    const copyTachesArray = [...taches];
+
+    //2. On manipule le tableau
+    const idUnique = new Date().getTime();
+    const updatedTachesArray = copyTachesArray.push({ id: idUnique, nom: event.target.value })
+    console.log(copyTachesArray)
+
+    //3. On met à jour le tableau
+    setTaches(updatedTachesArray);
+  }
+
+  const handleAdd = (event) => {
+    //Empêche le rechargement de la page.
+    event.preventDefault();
+  }
+
+  const handleDelete = (id) => {
+    // 3 Partie
+
+    //Copier le state
+    const copyTachesArray = [...taches];
+
+    //Manipuler
+    const updatedTachesArray = copyTachesArray.filter(tache => tache.id !== id)
+
+    //Updater
+    setTaches(updatedTachesArray);
+  }
 
   //Render
   return (
     <div>
       <h1>{titre}</h1>
       <ul>
-        {taches.map(tache => (
-          <li key={tache.id} > {tache.nom} </li>
-        ))}
+        {taches.map(tache =>
+          <li key={tache.id} > {tache.nom}
+            <button onClick={() => handleDelete(tache.id)}> Terminé ! </button>
+          </li>
+
+        )}
       </ul>
       <form>
-        <input type="text" placeholder='...' />
-        <button type='submit'>+</button>
+        <input type="text" placeholder='...' onChange={handleWrite} />
+        <button type='submit' onClick={handleAdd}>+</button>
       </form>
     </div>
   )
